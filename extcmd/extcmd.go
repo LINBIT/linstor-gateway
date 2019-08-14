7569d8368356b3b7e3061df4178fd763fe51f4a9
Roland Kammerer <roland.kammerer@linbit.com>
2019-08-14 11:16:19 +0200

    extcmd: add godoc

--- extcmd/extcmd.go
+++ extcmd/extcmd.go
@@ -1,3 +1,4 @@
+// Package extcmd is a simple wrapper around os/exec.
 package extcmd
 
 import (
@@ -6,6 +7,8 @@ import (
 	"os/exec"
 )
 
+// Execute executes a command that optionally takes a string that is sent to the command's stdin
+// The command returns stdout and stderr as strings.
 func Execute(forStdin *string, name string, arg ...string) (string, string, error) {
 	cmd := exec.Command(name, arg...)
 

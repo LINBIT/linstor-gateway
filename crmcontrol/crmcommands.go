ce6b0f7f798b5f6e4f0f584ac20b5d8dcba22d74
Roland Kammerer <roland.kammerer@linbit.com>
2019-08-14 15:55:04 +0200

    remove unnecessary type

--- crmcontrol/crmcommands.go
+++ crmcontrol/crmcommands.go
@@ -12,10 +12,10 @@ const (
 )
 
 // Command for creating new resources
-var CRM_CREATE_COMMAND CrmCommand = CrmCommand{CRM_UTILITY, []string{"--modify", "--allow-create", "--xml-pipe"}}
+var CRM_CREATE_COMMAND = CrmCommand{CRM_UTILITY, []string{"--modify", "--allow-create", "--xml-pipe"}}
 
 // Command for updating existing resources; also used for deleting existing resources
-var CRM_UPDATE_COMMAND CrmCommand = CrmCommand{CRM_UTILITY, []string{"--replace", "--xml-pipe"}}
+var CRM_UPDATE_COMMAND = CrmCommand{CRM_UTILITY, []string{"--replace", "--xml-pipe"}}
 
 // Command for reading the CIB
-var CRM_LIST_COMMAND CrmCommand = CrmCommand{CRM_UTILITY, []string{"--query"}}
+var CRM_LIST_COMMAND = CrmCommand{CRM_UTILITY, []string{"--query"}}

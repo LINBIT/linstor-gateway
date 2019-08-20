d6cc8b66c20675e70183c64351a8ad3529265de2
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2019-08-20 11:45:23 +0200

    cmd/root: adapt IQN regex to be compliant with backend

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -16,6 +17,24 @@ var lun int
 var loglevel string
 
 var controller net.IP // create and delete
+
+const (
+	// This format is currently dictated by the iSCSI target backend,
+	// specifically the rtslib-fb library.
+	// A notable difference in this implementation (which also differs from
+	// RFC3720, where the IQN format is defined) is that we require the
+	// "unique" part after the colon to be present.
+	//
+	// See also the source code of rtslib-fb for the original regex:
+	// https://github.com/open-iscsi/rtslib-fb/blob/b5be390be961/rtslib/utils.py#L384
+	regexIQN = `iqn\.\d{4}-[0-1][0-9]\..*\..*`
+
+	// This format is mandated by LINSTOR. Since we use the unique part
+	// directly for LINSTOR resource names, it needs to be compliant.
+	regexResourceName = `[[:alpha:]][[:alnum:]]+`
+
+	regexWWN = `^` + regexIQN + `:` + regexResourceName + `$`
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -34,11 +53,15 @@ as well as Corosync and Pacemaker's properties a prerequisite to use this tool.`
 		}
 		log.SetLevel(level)
 
-		matched, err := regexp.MatchString(`^iqn\.\d{4}-\d{2}\..*:.*`, iqn)
+		if strings.ContainsAny(iqn, "_ ") {
+			log.Fatal("IQN cannot contain the characters '_' (underscore) or ' ' (space)")
+		}
+
+		matched, err := regexp.MatchString(regexWWN, iqn)
 		if err != nil {
 			log.Fatal(err)
 		} else if !matched {
-			log.Fatal("Given IQN does not match specification")
+			log.Fatal("Given IQN does not match the regular expression: " + regexWWN)
 		}
 
 		if lun < 0 || lun > 255 {

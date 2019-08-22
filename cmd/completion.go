06928c0279cf407df20ef68cbea085a580318221
Roland Kammerer <roland.kammerer@linbit.com>
2019-08-22 10:39:23 +0200

    cmd: add (bash)completion

---
+++ cmd/completion.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// completionCmd represents the completion command
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generates bash completion script",
+	Long: `To load completion run
+
+. <(linstor-iscsi completion)
+
+To configure your bash shell to load completions for each session add to your bashrc
+
+# ~/.bashrc or ~/.profile
+. <(linstor-iscsi completion)`,
+	Run: func(cmd *cobra.Command, args []string) {
+		rootCmd.GenBashCompletion(os.Stdout)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}

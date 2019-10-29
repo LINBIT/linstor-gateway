982e26f4f827501d0902ada0f9bf510152c9321e
Roland Kammerer <roland.kammerer@linbit.com>
2019-10-29 14:35:36 +0100

    add routes.go

---
+++ pkg/rest/routes.go
@@ -0,0 +1,7 @@
+package rest
+
+func (s *server) routes() {
+	s.router.HandleFunc("/api/v1/iscsi", s.ISCSICreate()).Methods("POST")
+	s.router.HandleFunc("/api/v1/iscsi/{iqn}/{lun}", s.ISCSIDelete()).Methods("DELETE")
+	s.router.HandleFunc("/api/v1/iscsi/{iqn}/{lun}", s.ISCSIStatus()).Methods("GET")
+}

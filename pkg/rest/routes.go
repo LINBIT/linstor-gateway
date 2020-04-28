3f489e6af003d4d6976e6821ee21a67a86d3ae23
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2020-04-28 11:35:57 +0200

    rest: endpoints for starting and stopping targets

--- pkg/rest/routes.go
+++ pkg/rest/routes.go
@@ -5,4 +5,6 @@ func (s *server) routes() {
 	s.router.HandleFunc("/api/v1/iscsi", s.ISCSIList()).Methods("GET")
 	s.router.HandleFunc("/api/v1/iscsi/{iqn}/{lun}", s.ISCSIDelete()).Methods("DELETE")
 	s.router.HandleFunc("/api/v1/iscsi/{iqn}/{lun}", s.ISCSIStatus()).Methods("GET")
+	s.router.HandleFunc("/api/v1/iscsi/{iqn}/{lun}/start", s.ISCSIStart()).Methods("POST")
+	s.router.HandleFunc("/api/v1/iscsi/{iqn}/{lun}/stop", s.ISCSIStop()).Methods("POST")
 }

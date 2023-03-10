19015a49012201730bf8e8de78d21367cb98a51c
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2023-03-10 12:55:12 +0100

    new integration test: create-same-ip

---
+++ integration-tests/tests/create-same-ip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# create-same-ip
+# - Create an iSCSI resource
+# - Try to create an NVMe resource with the same IP address (should fail)
+
+from subprocess import CalledProcessError
+
+import gatewaytest
+
+nodes = gatewaytest.setup()
+
+first = nodes[0]
+first.start_server()
+
+first.run([
+    'linstor-gateway', 'iscsi', 'create', 'iqn.2019-08.com.linbit:iscsi1',
+    '192.168.122.220/24', '1G'
+])
+
+try:
+    first.run([
+        'linstor-gateway', 'nvme', 'create', 'nqn.2021-08.com.linbit:nvme:nvme1',
+        '192.168.122.220/24', '1G'
+    ])
+except CalledProcessError:
+    print("command threw CalledProcessError, that was expected")
+except BaseException:
+    raise
+finally:
+    nodes.cleanup()

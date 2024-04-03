91cc5e14eb0442691f6676d724592f37c3c1d595
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2024-04-03 14:50:22 +0200

    e2e-tests: upgrade to rhel9
    And install scst

--- integration-tests/tests/iscsi-create.py
+++ integration-tests/tests/iscsi-create.py
@@ -9,7 +9,7 @@ first.start_server()
 service_ip = nodes.get_service_ip()
 
 first.run([
-    'linstor-gateway', 'iscsi', 'create', 'iqn.2019-08.com.linbit:iscsi1',
+    'linstor-gateway', 'iscsi', 'create', '--implementation=lio-t', 'iqn.2019-08.com.linbit:iscsi1',
     service_ip, '1G',
 ])
 first.assert_resource_exists('iscsi', 'iqn.2019-08.com.linbit:iscsi1')

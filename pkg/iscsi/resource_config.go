LINBIT/linstor-gateway

commit bb53e397597d5a55230763710e680d43dce3ed46

iscsi: port to drbd-reactor

pkg/iscsi/resource_config.go
@@ -1,0 +1,305 @@
+package iscsi
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/LINBIT/golinstor/client"
+
+	"github.com/LINBIT/linstor-gateway/pkg/common"
+	"github.com/LINBIT/linstor-gateway/pkg/reactor"
+)
+
+const (
+	DefaultISCSIPort = 3260
+)
+
+type ResourceConfig struct {
+	IQN               Iqn                   `json:"iqn"`
+	AllowedInitiators []Iqn                 `json:"allowed_initiators,omitempty"`
+	ResourceGroup     string                `json:"resource_group"`
+	Volumes           []common.VolumeConfig `json:"volumes"`
+	Username          string                `json:"username,omitempty"`
+	Password          string                `json:"password,omitempty"`
+	ServiceIP         common.IpCidr         `json:"service_ip"`
+	Status            common.ResourceStatus `json:"status"`
+}
+
+func FromPromoter(cfg *reactor.PromoterConfig, definition *client.ResourceDefinition, volumeDefinition []client.VolumeDefinition) (*ResourceConfig, error) {
+	r := &ResourceConfig{}
+	var res string
+	n, err := fmt.Sscanf(cfg.ID, IDFormat, &res)
+	if n != 1 {
+		return nil, fmt.Errorf("failed to parse id into resource name: %w", err)
+	}
+
+	r.ResourceGroup = definition.ResourceGroupName
+
+	if len(cfg.Resources) != 1 {
+		return nil, errors.New(fmt.Sprintf("promoter config without exactly 1 resource (has %d)", len(cfg.Resources)))
+	}
+
+	var rscCfg reactor.PromoterResourceConfig
+	for _, v := range cfg.Resources {
+		rscCfg = v
+	}
+
+	if len(rscCfg.Start) < 4 {
+		return nil, errors.New(fmt.Sprintf("config has to few agent entries, expected at least 3, got %d", len(rscCfg.Start)))
+	}
+
+	portBlock := &rscCfg.Start[0]
+	if portBlock.Type != "ocf:heartbeat:portblock" {
+		return nil, errors.New(fmt.Sprintf("expected 'ocf:heartbeat:portblock' agent, got '%s' instead", portBlock.Type))
+	}
+
+	ipAgent := &rscCfg.Start[1]
+	if ipAgent.Type != "ocf:heartbeat:IPaddr2" {
+		return nil, errors.New(fmt.Sprintf("expected 'ocf:heartbeat:IPaddr2' agent, got '%s' instead", ipAgent.Type))
+	}
+
+	ip := net.ParseIP(ipAgent.Attributes["ip"])
+	if ip == nil {
+		return nil, fmt.Errorf("malformed ip %s", ipAgent.Attributes["ip"])
+	}
+
+	prefixLength, err := strconv.Atoi(ipAgent.Attributes["cidr_netmask"])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse service ip prefix")
+	}
+
+	r.ServiceIP = common.ServiceIPFromParts(ip, prefixLength)
+
+	targetAgent := &rscCfg.Start[2]
+	if targetAgent.Type != "ocf:heartbeat:iSCSITarget" {
+		return nil, errors.New(fmt.Sprintf("expected 'ocf:heartbeat:iSCSITarget' agent, got '%s' instead", targetAgent.Type))
+	}
+
+	r.IQN, err = NewIqn(targetAgent.Attributes["iqn"])
+	if err != nil {
+		return nil, fmt.Errorf("got malformed iqn: %w", err)
+	}
+
+	r.Username = targetAgent.Attributes["incoming_username"]
+	r.Password = targetAgent.Attributes["incoming_password"]
+
+	rawAllowed := targetAgent.Attributes["allowed_initiators"]
+	if rawAllowed != "" {
+		for _, allowed := range strings.Split(rawAllowed, " ") {
+			iqn, err := NewIqn(allowed)
+			if err != nil {
+				return nil, fmt.Errorf("got malformed iqn %s for allowed initiators: %w", allowed, err)
+			}
+			r.AllowedInitiators = append(r.AllowedInitiators, iqn)
+		}
+	}
+
+	for _, vd := range volumeDefinition {
+		r.Volumes = append(r.Volumes, common.VolumeConfig{
+			Number:  int(vd.VolumeNumber),
+			SizeKiB: vd.SizeKib,
+		})
+	}
+
+	return r, nil
+}
+
+func (r *ResourceConfig) VolumeConfig(number int) *common.Volume {
+	var result *common.Volume
+
+	for _, vol := range r.Volumes {
+		if vol.Number == number {
+			result = &common.Volume{
+				Volume: vol,
+			}
+			break
+		}
+	}
+
+	for _, volState := range r.Status.Volumes {
+		if volState.Number == number {
+			if result == nil {
+				result = &common.Volume{}
+			}
+
+			result.Status = volState
+			break
+		}
+	}
+
+	return result
+}
+
+func (r *ResourceConfig) FillDefaults() {
+	if r.ResourceGroup == "" {
+		r.ResourceGroup = "DfltRscGrp"
+	}
+
+	for i := range r.Volumes {
+		if r.Volumes[i].Number == 0 {
+			r.Volumes[i].Number = i + 1
+		}
+	}
+}
+
+func (r *ResourceConfig) Valid() error {
+	if len(r.IQN.WWN()) < 2 {
+		return common.ValidationError("iscsi wwn string to short (min. 2)")
+	}
+
+	if r.ServiceIP.IP() == nil {
+		return common.ValidationError("missing service ip")
+	}
+
+	if r.ServiceIP.Mask == nil {
+		return common.ValidationError("missing service ip prefix length")
+	}
+
+	sort.Slice(r.Volumes, func(i, j int) bool {
+		return r.Volumes[i].Number < r.Volumes[j].Number
+	})
+
+	for i := range r.Volumes {
+		if r.Volumes[i].Number < 1 {
+			return common.ValidationError("volume numbers must start at 1")
+		}
+
+		if r.Volumes[i].SizeKiB <= 0 {
+			return common.ValidationError("volume size must be positive")
+		}
+
+		if i > 0 && r.Volumes[i-1].Number == r.Volumes[i].Number {
+			return common.ValidationError("volume numbers must be unique")
+		}
+	}
+
+	return nil
+}
+
+func (r *ResourceConfig) Matches(o *ResourceConfig) bool {
+	if r.IQN != o.IQN {
+		return false
+	}
+
+	if r.ServiceIP.String() != o.ServiceIP.String() {
+		return false
+	}
+
+	if r.ResourceGroup != o.ResourceGroup {
+		return false
+	}
+
+	if len(r.Volumes) != len(o.Volumes) {
+		return false
+	}
+
+	for i := range r.Volumes {
+		if r.Volumes[i].Number != o.Volumes[i].Number {
+			return false
+		}
+
+		if r.Volumes[i].SizeKiB != o.Volumes[i].SizeKiB {
+			return false
+		}
+	}
+
+	if r.Username != o.Username {
+		return false
+	}
+
+	if r.Password != o.Password {
+		return false
+	}
+
+	return true
+}
+
+func (r *ResourceConfig) portal() string {
+	return fmt.Sprintf("%s:%d", r.ServiceIP.IP(), DefaultISCSIPort)
+}
+
+func (r *ResourceConfig) ID() string {
+	return fmt.Sprintf(IDFormat, r.IQN.WWN())
+}
+
+func (r *ResourceConfig) ToPromoter(deployment []client.ResourceWithVolumes) (*reactor.PromoterConfig, error) {
+	if len(deployment) == 0 {
+		return nil, errors.New("resource config is missing deployment information")
+	}
+
+	allowedInitiatorStrings := make([]string, 0, len(r.AllowedInitiators))
+	for i := range r.AllowedInitiators {
+		allowedInitiatorStrings = append(allowedInitiatorStrings, r.AllowedInitiators[i].String())
+	}
+
+	agents := []reactor.ResourceAgent{
+		{Type: "ocf:heartbeat:portblock", Name: "pblock", Attributes: map[string]string{"ip": r.ServiceIP.IP().String(), "portno": strconv.Itoa(DefaultISCSIPort), "action": "block", "protocol": "tcp"}},
+		{Type: "ocf:heartbeat:IPaddr2", Name: "service_ip", Attributes: map[string]string{"ip": r.ServiceIP.IP().String(), "cidr_netmask": strconv.Itoa(r.ServiceIP.Prefix())}},
+		{
+			Type: "ocf:heartbeat:iSCSITarget", Name: "target", Attributes: map[string]string{
+				"iqn":                r.IQN.String(),
+				"portals":            r.portal(),
+				"incoming_username":  r.Username,
+				"incoming_password":  r.Password,
+				"allowed_initiators": strings.Join(allowedInitiatorStrings, " "),
+			},
+		},
+	}
+
+	for i, vol := range deployment[0].Volumes {
+		if int(vol.VolumeNumber) != r.Volumes[i].Number {
+			return nil, fmt.Errorf("inconsistent volumes, expected volume number %d, got %d", vol.VolumeNumber, r.Volumes[i].Number)
+		}
+
+		devPath := vol.DevicePath
+		for k, v := range vol.Props {
+			if strings.HasPrefix(k, "Satellite/Device/Symlinks/") {
+				devPath = v
+			}
+
+			// Prefer the "by-res" symlinks
+			if strings.Contains(v, "/by-res/") {
+				break
+			}
+		}
+
+		agents = append(agents, reactor.ResourceAgent{
+			Type: "ocf:heartbeat:iSCSILogicalUnit",
+			Name: fmt.Sprintf("lu%d", vol.VolumeNumber),
+			Attributes: map[string]string{
+				"target_iqn": r.IQN.String(),
+				"lun":        strconv.Itoa(int(vol.VolumeNumber)),
+				"path":       fmt.Sprintf(devPath),
+				"product_id": "LINSTOR iSCSI",
+			},
+		})
+	}
+
+	agents = append(agents, reactor.ResourceAgent{
+		Type: "ocf:heartbeat:portblock",
+		Name: "portunblock",
+		Attributes: map[string]string{
+			"ip":       r.ServiceIP.IP().String(),
+			"portno":   strconv.Itoa(DefaultISCSIPort),
+			"action":   "unblock",
+			"protocol": "tcp",
+		},
+	})
+
+	return &reactor.PromoterConfig{
+		ID: r.ID(),
+		Resources: map[string]reactor.PromoterResourceConfig{
+			r.IQN.WWN(): {
+				Runner:             "systemd",
+				Start:              agents,
+				StopServicesOnExit: true,
+				OnStopFailure:      "echo b > /proc/sysrq-trigger",
+				TargetAs:           "Requires",
+			},
+		},
+	}, nil
+}

cmd/corosync.go
@@ -1,40 +1,0 @@
-package cmd
-
-import (
-	"net"
-
-	corosync "github.com/LINBIT/gocorosync"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-)
-
-func corosyncCommand() *cobra.Command {
-	var nodeIPs []net.IP
-	var clusterName string
-
-	var corosyncCmd = &cobra.Command{
-		Use:   "corosync",
-		Short: "Generates a corosync config",
-		Long: `Generates a corosync config
-
-For example:
-linstor-iscsi corosync --ips="192.168.1.1,192.168.1.2"`,
-		Args: cobra.NoArgs,
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(nodeIPs) == 0 {
-				log.Fatal("IP list is empty")
-			}
-			corosync.GenerateConfig(nodeIPs, clusterName)
-		},
-	}
-
-	corosyncCmd.ResetCommands()
-	corosyncCmd.Flags().IPSliceVar(&nodeIPs, "ips", []net.IP{net.IPv4(127, 0, 0, 1)}, "comma seprated list of IPs (e.g., 1.2.3.4,1.2.3.5)")
-	corosyncCmd.Flags().StringVar(&clusterName, "cluster-name", "mycluster", "name of the cluster")
-
-	corosyncCmd.MarkFlagRequired("iqn")
-	corosyncCmd.MarkFlagRequired("lun")
-	corosyncCmd.DisableAutoGenTag = true
-
-	return corosyncCmd
-}

cmd/create_iscsi.go.disabled
@@ -1,125 +1,0 @@
-package cmd
-
-import (
-	"net"
-	"strconv"
-
-	"github.com/LINBIT/gopacemaker/cib"
-	"github.com/LINBIT/linstor-gateway/pkg/crmcontrol"
-	"github.com/LINBIT/linstor-gateway/pkg/iscsi"
-	"github.com/LINBIT/linstor-gateway/pkg/linstorcontrol"
-	"github.com/LINBIT/linstor-gateway/pkg/targetutil"
-	"github.com/rck/unit"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-)
-
-func createISCSICommand() *cobra.Command {
-	var ipCIDR string
-	var controller net.IP
-	var username, password, portals, group string
-	var iqn string
-	var lun int
-
-	var sz *unit.Value
-	var sizeKiB uint64
-	var ip net.IP
-	var ipnet *net.IPNet
-
-	var createCmd = &cobra.Command{
-		Use:   "create",
-		Short: "Creates an iSCSI target",
-		Long: `Creates a highly available iSCSI target based on LINSTOR and Pacemaker.
-At first it creates a new resouce within the linstor system, using the
-specified resource group. The name of the linstor resources is derived
-from the iqn and the lun number.
-After that it creates resource primitives in the Pacemaker cluster including
-all necessary order and location constraints. The Pacemaker primites are
-prefixed with p_, contain the name and a resource type postfix.
-
-For example:
-linstor-iscsi create --iqn=iqn.2019-08.com.linbit:example --ip=192.168.122.181/24 \
- -username=foo --lun=1 --password=bar --resource-group=ssd_thin_2way --size=2G
-
-Creates linstor resources example_lu0 and
-pacemaker primitives p_iscsi_example_ip, p_iscsi_example, p_iscsi_example_lu0`,
-
-		Args: cobra.NoArgs,
-		PreRun: func(cmd *cobra.Command, args []string) {
-			sizeKiB = uint64(sz.Value / unit.DefaultUnits["K"])
-
-			var err error
-			ip, ipnet, err = net.ParseCIDR(ipCIDR)
-			if err != nil {
-				log.Fatalf("Invalid service IP: %s", err.Error())
-			}
-
-			if portals == "" {
-				portals = ip.String() + ":" + strconv.Itoa(iscsi.DFLT_ISCSI_PORTAL_PORT)
-			}
-		},
-
-		Run: func(cmd *cobra.Command, args []string) {
-			if !cmd.Flags().Changed("controller") {
-				foundIP, err := crmcontrol.FindLinstorController()
-				if err == nil { // it might be ok to not find it...
-					controller = foundIP
-				} else if err == cib.ErrCibFailed {
-					log.Fatal(err)
-				}
-			}
-			linstorCfg := linstorcontrol.Linstor{
-				ControllerIP:      controller,
-				ResourceGroupName: group,
-				SizeKiB:           sizeKiB,
-			}
-
-			ones, _ := ipnet.Mask.Size()
-
-			targetCfg := targetutil.TargetConfig{
-				LUNs:             []*targetutil.LUN{&targetutil.LUN{ID: uint8(lun)}},
-				IQN:              iqn,
-				ServiceIP:        ip,
-				ServiceIPNetmask: ones,
-				Username:         username,
-				Password:         password,
-				Portals:          portals,
-			}
-			target := cliNewTargetMust(cmd, targetCfg)
-			iscsiCfg := &iscsi.ISCSI{Linstor: linstorCfg, Target: target}
-			err := iscsiCfg.CreateResource()
-			if err != nil {
-				log.Fatal(err)
-			}
-		},
-	}
-
-	createCmd.Flags().StringVarP(&iqn, "iqn", "i", "", "Set the iSCSI Qualified Name (e.g., iqn.2019-08.com.linbit:unique) (required)")
-	createCmd.Flags().IntVarP(&lun, "lun", "l", 1, "Set the LUN Number (required)")
-	createCmd.Flags().StringVar(&ipCIDR, "ip", "127.0.0.1/8", "Set the service IP and netmask of the target (required)")
-	createCmd.Flags().IPVarP(&controller, "controller", "c", net.IPv4(127, 0, 0, 1), "Set the IP of the linstor controller node")
-	createCmd.Flags().StringVar(&portals, "portals", "", "Set up portals, if unset, the service ip and default port")
-	createCmd.Flags().StringVarP(&username, "username", "u", "", "Set the username (required)")
-	createCmd.Flags().StringVarP(&password, "password", "p", "", "Set the password (required)")
-
-	units := unit.DefaultUnits
-	units["KiB"] = units["K"]
-	units["MiB"] = units["M"]
-	units["GiB"] = units["G"]
-	units["TiB"] = units["T"]
-	units["PiB"] = units["P"]
-	units["EiB"] = units["E"]
-	u := unit.MustNewUnit(units)
-	sz = u.MustNewValue(1*units["G"], unit.None)
-	createCmd.Flags().Var(sz, "size", "Set a size (e.g, 1TiB)")
-
-	createCmd.Flags().StringVarP(&group, "resource-group", "g", "DfltRscGrp", "Set the LINSTOR resource-group")
-
-	createCmd.MarkFlagRequired("ip")
-	createCmd.MarkFlagRequired("username")
-	createCmd.MarkFlagRequired("password")
-	createCmd.MarkFlagRequired("iqn")
-	createCmd.MarkFlagRequired("lun")
-
-	return createCmd
-}

cmd/delete_iscsi.go.disabled
@@ -1,58 +1,0 @@
-package cmd
-
-import (
-	"net"
-
-	"github.com/LINBIT/linstor-gateway/pkg/crmcontrol"
-	"github.com/LINBIT/linstor-gateway/pkg/iscsi"
-	"github.com/LINBIT/linstor-gateway/pkg/linstorcontrol"
-	"github.com/LINBIT/linstor-gateway/pkg/targetutil"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-)
-
-func deleteISCSICommand() *cobra.Command {
-	var controller net.IP
-	var iqn string
-	var lun int
-
-	var deleteCmd = &cobra.Command{
-		Use:   "delete",
-		Short: "Deletes an iSCSI target",
-		Long: `Deletes an iSCSI target by stopping and deleting the pacemaker resource
-primitives and removing the linstor resources.
-
-For example:
-linstor-iscsi delete --iqn=iqn.2019-08.com.linbit:example --lun=1`,
-		Args: cobra.NoArgs,
-		Run: func(cmd *cobra.Command, args []string) {
-			if !cmd.Flags().Changed("controller") {
-				foundIP, err := crmcontrol.FindLinstorController()
-				if err == nil { // it might be ok to not find it...
-					controller = foundIP
-				}
-			}
-			linstorCfg := linstorcontrol.Linstor{
-				ControllerIP: controller,
-			}
-			targetCfg := targetutil.TargetConfig{
-				IQN:  iqn,
-				LUNs: []*targetutil.LUN{&targetutil.LUN{ID: uint8(lun)}},
-			}
-			target := cliNewTargetMust(cmd, targetCfg)
-			iscsiCfg := &iscsi.ISCSI{Linstor: linstorCfg, Target: target}
-			if err := iscsiCfg.DeleteResource(); err != nil {
-				log.Fatal(err)
-			}
-		},
-	}
-
-	deleteCmd.Flags().StringVarP(&iqn, "iqn", "i", "", "Set the iSCSI Qualified Name (e.g., iqn.2019-08.com.linbit:unique) (required)")
-	deleteCmd.Flags().IntVarP(&lun, "lun", "l", 1, "Set the LUN Number (required)")
-	deleteCmd.Flags().IPVarP(&controller, "controller", "c", net.IPv4(127, 0, 0, 1), "Set the IP of the linstor controller node")
-
-	deleteCmd.MarkFlagRequired("iqn")
-	deleteCmd.MarkFlagRequired("lun")
-
-	return deleteCmd
-}

cmd/helpers.go
@@ -1,36 +1,0 @@
-package cmd
-
-import (
-	"os"
-
-	"github.com/LINBIT/linstor-gateway/pkg/targetutil"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-)
-
-// cliNewTargetMust is a simple, but helpful helper around the required iqn/lun parameters
-func cliNewTargetMust(cmd *cobra.Command, targetCfg targetutil.TargetConfig) targetutil.Target {
-	var errs []string
-	if !cmd.Flags().Changed("iqn") {
-		errs = append(errs, "Parameter '--iqn' is mandatory")
-	}
-	if !cmd.Flags().Changed("lun") {
-		errs = append(errs, "Parameter '--lun' is mandatory")
-	}
-
-	if len(errs) > 0 {
-		cmd.Help()
-		for _, e := range errs {
-			log.Errorln(e)
-		}
-		os.Exit(1)
-	}
-
-	return targetutil.NewTargetMust(targetCfg)
-}
-
-func lunMust(lun int) {
-	if lun < 1 || lun > 255 {
-		log.Fatal("LUN out of range [0-255]")
-	}
-}

cmd/iscsi.go
@@ -1,0 +1,234 @@
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/LINBIT/linstor-gateway/pkg/common"
+	"github.com/LINBIT/linstor-gateway/pkg/iscsi"
+	"github.com/olekukonko/tablewriter"
+	"github.com/rck/unit"
+	"github.com/spf13/cobra"
+)
+
+func createISCSICommand() *cobra.Command {
+	var username, password, portals, group string
+	var iqn iscsi.Iqn
+
+	var lun int
+
+	var sz *unit.Value
+	var serviceIp common.IpCidr
+
+	var createCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Creates an iSCSI target",
+		Long: `Creates a highly available iSCSI target based on LINSTOR and drbd-reactor.
+At first it creates a new resource within the LINSTOR system, using the
+specified resource group. The name of the linstor resources is derived
+from the IQN's World Wide Name, which must be unique'.
+After that it creates a configuration for drbd-reactor to manage the
+high availabilitiy primitives.`,
+		Example: "linstor-iscsi create --iqn=iqn.2019-08.com.linbit:example --ip=192.168.122.181/24 --username=foo --lun=1 --password=bar --resource-group=ssd_thin_2way --size=2G",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+
+			_, err := iscsi.Create(ctx, &iscsi.ResourceConfig{
+				IQN:       iqn,
+				Username:  username,
+				Password:  password,
+				ServiceIP: serviceIp,
+				Volumes: []common.VolumeConfig{
+					{Number: lun, SizeKiB: uint64(sz.Value / unit.K)},
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Created iSCSI target '%s'\n", iqn)
+
+			return nil
+		},
+	}
+
+	createCmd.Flags().Var(&serviceIp, "ip", "Set the service IP and netmask of the target")
+	createCmd.Flags().VarP(&iqn, "iqn", "i", "Set the iSCSI Qualified Name (e.g., iqn.2019-08.com.linbit:unique)")
+	createCmd.Flags().IntVarP(&lun, "lun", "l", 1, "Set the LUN")
+	createCmd.Flags().StringVar(&portals, "portals", "", "Set up portals, if unset, the service ip and default port")
+	createCmd.Flags().StringVarP(&username, "username", "u", "", "Set the username (required)")
+	createCmd.Flags().StringVarP(&password, "password", "p", "", "Set the password (required)")
+
+	units := unit.DefaultUnits
+	units["KiB"] = units["K"]
+	units["MiB"] = units["M"]
+	units["GiB"] = units["G"]
+	units["TiB"] = units["T"]
+	units["PiB"] = units["P"]
+	units["EiB"] = units["E"]
+	u := unit.MustNewUnit(units)
+	sz = u.MustNewValue(1*units["G"], unit.None)
+	createCmd.Flags().Var(sz, "size", "Set a size (e.g, 1TiB)")
+
+	createCmd.Flags().StringVarP(&group, "resource-group", "g", "DfltRscGrp", "Set the LINSTOR resource-group")
+
+	createCmd.MarkFlagRequired("ip")
+	createCmd.MarkFlagRequired("iqn")
+	createCmd.MarkFlagRequired("size")
+
+	return createCmd
+}
+
+func listISCSICommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "list",
+		Short: "Lists iSCSI targets",
+		Long: `Lists the iSCSI targets created with this tool and provides an overview
+about the existing drbd-reactor and linstor parts.`,
+		Example: "linstor-iscsi list",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfgs, err := iscsi.List(context.Background())
+			if err != nil {
+				return err
+			}
+
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"IQN", "Service IP", "Service state", "LUN", "LINSTOR state"})
+			table.SetHeaderColor(tableColorHeader, tableColorHeader, tableColorHeader, tableColorHeader, tableColorHeader)
+
+			for _, cfg := range cfgs {
+				for _, vol := range cfg.Status.Volumes {
+					table.Rich(
+						[]string{cfg.IQN.String(), cfg.ServiceIP.String(), cfg.Status.Service.String(), strconv.Itoa(vol.Number), vol.State.String()},
+						[]tablewriter.Colors{{}, {}, ServiceStateColor(cfg.Status.Service), {}, ResourceStateColor(vol.State)},
+					)
+				}
+			}
+
+			table.SetAutoMergeCellsByColumnIndex([]int{0, 1})
+			table.SetAutoFormatHeaders(false)
+			table.Render()
+
+			return nil
+		},
+	}
+}
+
+func startISCSICommand() *cobra.Command {
+	var iqn iscsi.Iqn
+
+	var startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Starts an iSCSI target",
+		Long: `Sets the target role attribute of a Pacemaker primitive to started.
+In case it does not start use your favourite pacemaker tools to analyze
+the root cause.`,
+		Example: "linstor-iscsi start --iqn=iqn.2019-08.com.linbit:example",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg, err := iscsi.Start(context.Background(), iqn)
+			if err != nil {
+				return err
+			}
+
+			if cfg == nil {
+				return errors.New(fmt.Sprintf("Unknown target %s", iqn))
+			}
+
+			fmt.Printf("Started target %s\n", iqn)
+
+			return nil
+		},
+	}
+
+	startCmd.Flags().VarP(&iqn, "iqn", "i", "Set the iSCSI Qualified Name (e.g., iqn.2019-08.com.linbit:unique)")
+
+	startCmd.MarkFlagRequired("iqn")
+
+	return startCmd
+}
+
+func stopISCSICommand() *cobra.Command {
+	var iqn iscsi.Iqn
+
+	var stopCmd = &cobra.Command{
+		Use:   "stop",
+		Short: "Stops an iSCSI target",
+		Long: `Sets the target role attribute of a Pacemaker primitive to stopped.
+This causes pacemaker to stop the components of an iSCSI target.
+
+For example:
+linstor-iscsi stop --iqn=iqn.2019-08.com.linbit:example`,
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg, err := iscsi.Stop(context.Background(), iqn)
+			if err != nil {
+				return err
+			}
+
+			if cfg == nil {
+				return errors.New(fmt.Sprintf("Unknown target %s", iqn))
+			}
+
+			fmt.Printf("Stopped target %s\n", iqn)
+
+			return nil
+		},
+	}
+
+	stopCmd.Flags().VarP(&iqn, "iqn", "i", "Set the iSCSI Qualified Name (e.g., iqn.2019-08.com.linbit:unique) (required)")
+
+	stopCmd.MarkFlagRequired("iqn")
+
+	return stopCmd
+}
+
+func deleteISCSICommand() *cobra.Command {
+	var iqn iscsi.Iqn
+	var lun int
+
+	var deleteCmd = &cobra.Command{
+		Use:   "delete",
+		Short: "Deletes an iSCSI target",
+		Long: `Deletes an iSCSI target by stopping and deleting the pacemaker resource
+primitives and removing the linstor resources.`,
+		Example: "linstor-iscsi delete --iqn=iqn.2019-08.com.linbit:example --lun=1",
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			if cmd.Flags().Changed("lun") {
+				cfg, err := iscsi.DeleteVolume(ctx, iqn, lun)
+				if err != nil {
+					return err
+				}
+
+				if cfg == nil {
+					return errors.New(fmt.Sprintf("Unknown target %s\n", iqn))
+				} else {
+					fmt.Printf("Deleted LU %d for target %s\n", lun, iqn)
+				}
+			} else {
+				err := iscsi.Delete(ctx, iqn)
+				if err != nil {
+					return err
+				}
+
+				fmt.Printf("Deleted target %s\n", iqn)
+			}
+
+			return nil
+		},
+	}
+
+	deleteCmd.Flags().VarP(&iqn, "iqn", "i", "The iSCSI Qualified Name (e.g., iqn.2019-08.com.linbit:unique) of the target to delete.")
+	deleteCmd.Flags().IntVarP(&lun, "lun", "l", -1, "Set the LUN")
+
+	deleteCmd.MarkFlagRequired("iqn")
+
+	return deleteCmd
+}

cmd/list_iscsi.go.disabled
@@ -1,111 +1,0 @@
-package cmd
-
-import (
-	"net"
-	"os"
-	"strconv"
-
-	"github.com/LINBIT/linstor-gateway/pkg/crmcontrol"
-	"github.com/LINBIT/linstor-gateway/pkg/iscsi"
-	"github.com/LINBIT/linstor-gateway/pkg/linstorcontrol"
-	"github.com/LINBIT/linstor-gateway/pkg/targetutil"
-	"github.com/mattn/go-isatty"
-	"github.com/olekukonko/tablewriter"
-	log "github.com/sirupsen/logrus"
-
-	"github.com/spf13/cobra"
-)
-
-func listISCSICommand() *cobra.Command {
-	var controller net.IP
-	var listCmd = &cobra.Command{
-		Use:   "list",
-		Short: "Lists iSCSI targets",
-		Long: `Lists the iSCSI targets created with this tool and provides an overview
-about the existing Pacemaker and linstor parts
-
-For example:
-linstor-iscsi list`,
-		Args: cobra.NoArgs,
-		Run: func(cmd *cobra.Command, args []string) {
-			if !cmd.Flags().Changed("controller") {
-				foundIP, err := crmcontrol.FindLinstorController()
-				if err == nil { // it might be ok to not find it...
-					controller = foundIP
-					log.Debugf("Found LINSTOR controller at %s", foundIP)
-				} else {
-					log.Debugf("Could not find LINSTOR controller in CIB")
-				}
-			}
-			log.Debugf("Using LINSTOR controller at %s", controller)
-			targets, err := iscsi.ListResources()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			table := tablewriter.NewWriter(os.Stdout)
-			table.SetHeader([]string{"IQN", "LUN", "Pacemaker LUN", "Pacemaker", "Pacemaker IP", "LINSTOR"})
-
-			if isatty.IsTerminal(os.Stdout.Fd()) {
-				whiteBold := tablewriter.Colors{tablewriter.FgBlueColor, tablewriter.Bold}
-				table.SetHeaderColor(whiteBold, whiteBold, whiteBold, whiteBold, whiteBold, whiteBold)
-			}
-
-			for _, target := range targets {
-				linstorCfg := linstorcontrol.Linstor{
-					ControllerIP: controller,
-				}
-				targetCfg := targetutil.TargetConfig{
-					IQN:  target.IQN,
-					LUNs: target.LUNs,
-				}
-				tgt := targetutil.NewTargetMust(targetCfg)
-				iscsiCfg := &iscsi.ISCSI{Linstor: linstorCfg, Target: tgt}
-
-				resourceState, err := iscsiCfg.ProbeResource()
-				if err != nil {
-					log.WithFields(log.Fields{
-						"iqn": target.IQN,
-					}).Warning("Cannot probe target: ", err)
-				}
-
-				for _, lu := range target.LUNs {
-					targetName, err := targetutil.ExtractTargetName(target.IQN)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					linstorCfg.ResourceName = linstorcontrol.ResourceNameFromLUN(targetName, lu.ID)
-					targetState := resourceState.TargetState
-					lunState := resourceState.LUStates[lu.ID]
-					ipState := resourceState.IPState
-					linstorState, err := linstorCfg.AggregateResourceState()
-					if err != nil {
-						log.Warning(err)
-						linstorState = linstorcontrol.Unknown
-					}
-
-					row := []string{
-						target.IQN,
-						strconv.Itoa(int(lu.ID)),
-						stateToStatus(lunState),
-						stateToStatus(targetState),
-						stateToStatus(ipState),
-						linstorStateToStatus(linstorState),
-					}
-					table.Append(row)
-				}
-			}
-
-			// TODO this would look cool, but it would merge the ticks too...
-			//table.SetAutoMergeCells(true)
-			table.SetAutoFormatHeaders(false)
-			table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_RIGHT, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER, tablewriter.ALIGN_CENTER})
-
-			table.Render() // Send output
-		},
-	}
-
-	listCmd.Flags().IPVarP(&controller, "controller", "c", net.IPv4(127, 0, 0, 1), "Set the IP of the linstor controller node")
-	return listCmd
-}

commit 207d99787626176b1bee1d989c07a792f8702688

fix go vet warnings for non-constant format strings
Remove unnecessary fmt.Sprintf() wrappers around plain string
variables. These were flagged by go vet because passing a variable
as a format string could cause issues if it accidentally contained
format specifiers.

Fixes: bb53e397 ("iscsi: port to drbd-reactor")
Fixes: 50108db2 ("add nvme target")
Co-Authored-By: Claude Opus 4.5 <noreply@anthropic.com>

pkg/iscsi/resource_config.go
@@ -365,7 +365,7 @@ func (r *ResourceConfig) ToPromoter(deployment []client.ResourceWithVolumes) (*r
 		luAttrs := map[string]string{
 			"target_iqn": r.IQN.String(),
 			"lun":        strconv.Itoa(int(vol.VolumeNumber)),
-			"path":       fmt.Sprintf(devPath),
+			"path":       devPath,
 			// Attention: VMware uses the product_id (= t10_dev_id) to determine
 			// LUN equivalence, so it must be unique for each LUN. However, it
 			// only considers the first 8 bytes for whatever reason, so take

pkg/nvmeof/resource_config.go
@@ -214,7 +214,7 @@ func (r *ResourceConfig) ToPromoter(deployment []client.ResourceWithVolumes) (*r
 				"nqn": r.NQN.String(),
 				// nvme namespaces start at 1, we just ensure that our volumes also start at 1
 				"namespace_id": fmt.Sprintf("%d", vol.VolumeNumber),
-				"backing_path": fmt.Sprintf(devPath),
+				"backing_path": devPath,
 				"uuid":         guid.String(),
 				"nguid":        guid.String(),
 			},